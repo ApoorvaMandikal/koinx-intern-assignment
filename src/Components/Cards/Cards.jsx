4944d53d38eb8de5a44cedd05cf750fdd8d14e79
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-03-06 17:16:24 +0530

    Creation of components + API key

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,10 +1,1 @@
-import logo from './logo.svg';
-import './App.css';
-
 const API_URL = 'https://pro-api.coingecko.com/api/v3/ping?x_cg_pro_api_key=CG-xkRg7TR313TXEL3G5wva43HL'	
-
-function App() {
- 
-}
-
-export default App;
